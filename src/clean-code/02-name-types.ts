10376f29246772ee6323409f03ddcdd1cb174551
smarcasco <sergito91@gmail.com>
2026-07-21 10:48:16 +0200

    Nombres por tipos.

--- src/clean-code/02-name-types.ts
+++ src/clean-code/02-name-types.ts
@@ -7,10 +7,14 @@
     const serverIp = '123.123.123.123';
 
     // Listado de usuarios
-    const users = [{id: 1, email: 'fernando@google.com'},{ id: 2, email: 'juan@google.com' }, { id: 3, email: 'melissa@google.com' }];
+    const users = [
+        {id: 1, email: 'fernando@google.com'},
+        { id: 2, email: 'juan@google.com' }, 
+        { id: 3, email: 'melissa@google.com' }
+    ];
 
     // Listado de emails de los usuarios
-    const userEmails = users.map( users => users.email );
+    const emailOfUsers = users.map( users => users.email );
 
     // Variables booleanas de un video juego
     const canJump = false;
